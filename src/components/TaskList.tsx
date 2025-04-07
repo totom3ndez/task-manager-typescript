a493a71b269486710b90b0bb2d54089babb0f9af
totom3ndez <tomii.mendez45@gmail.com>
2025-04-07 12:23:27 +0200

    feat: implement TaskList component for displaying tasks

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,25 @@
+import {taskStore} from "../taskStore";
+import { useNavigate } from "react-router-dom";
+
+import TaskItem from "./TaskItem";
+
+const TaskList = () => {
+  const navigate = useNavigate()
+  const tasks = taskStore((state) => state.tasks)
+  const selectTask = taskStore((state) => state.selectTask)
+  return (
+    <div >
+      <ul className="flex flex-col gap-4">
+      {
+        tasks.map(task => (
+          <li key={task.id} onClick={() => selectTask(task, navigate)}>
+            <TaskItem  task={task}/>
+          </li>
+        ))
+      }
+      </ul>
+    </div>
+  )
+}
+
+export default TaskList;
