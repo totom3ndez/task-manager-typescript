915c829563ad59d053e96b34c41e962ee738e4c8
totom3ndez <tomii.mendez45@gmail.com>
2025-04-07 12:23:23 +0200

    feat: create Layout component for consistent page structure

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+import { ReactNode } from 'react'
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+
+    <div className='mx-auto bg-[#101010] min-h-screen text-white'>
+      <Header />
+      <main>
+        {children}
+      </main>
+    </div>)
+}
+
+export default Layout
