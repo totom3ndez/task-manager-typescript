d9f783338bef3405638cb479611abb7a28e4be9c
totom3ndez <tomii.mendez45@gmail.com>
2025-05-12 16:02:15 +0200

    feat: restructure Home component layout to include sign-in functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,28 @@
 import TaskList from '../tasks/components/TaskList'
 import TaskHistory from '../tasks/components/TaskHistory'
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
+
 
 const Home = () => {
   return (
     <>
-      <TaskList />
-      <TaskHistory />
+      <div className='flex justify-between items-center p-4'>
+        <div className='flex gap-4'>
+          <SignedIn>
+            <div className='flex flex-col justify-center items-center'>
+              <div className='flex gap-4 align-center'>
+              </div>
+              <div>
+                <TaskList />
+                <TaskHistory />
+              </div>
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">Sign In</SignInButton>
+          </SignedOut>
+        </div>
+      </div>
     </>
   )
 }
