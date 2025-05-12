09a8792b25e12c08805089fc76f978c1e2d2ecb9
totom3ndez <tomii.mendez45@gmail.com>
2025-05-12 16:02:30 +0200

    refactor: simplify JSX structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Layout from "./layouts/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
+
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-task" element={<CreateTask />} />
-          <Route path='/task-details' element={<TaskDetails />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-task" element={<CreateTask />} />
+            <Route path='/task-details' element={<TaskDetails />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
