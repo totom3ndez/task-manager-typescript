910f816db7851d820be32a8c0da710961605794e
totom3ndez <tomii.mendez45@gmail.com>
2025-05-13 14:49:46 +0200

    style: refactor layout in Home component for improved structure and readability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,28 +6,24 @@ import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
 const Home = () => {
   return (
     <>
-      <div className='flex justify-between items-center p-4'>
-        <div className='flex gap-4'>
-          <SignedIn>
-            <div className='flex flex-col justify-center items-center'>
-              <div className='flex gap-4 align-center'>
-              </div>
-              <div>
-                <TaskList />
-                <TaskHistory />
-              </div>
+      <div className='flex gap-4 justify-center'>
+        <SignedIn>
+          <div className='flex flex-col justify-center items-center'>
+            <div>
+              <TaskList />
+              <TaskHistory />
             </div>
-          </SignedIn>
-          <SignedOut>
-            <div className='items-center flex gap-2'>
-              <p>Hello guest 👋, please </p>
-              <div className='p-2 bg-blue-700 rounded-xl text-white cursor-pointer'>
-                <SignInButton mode="modal">Sign in</SignInButton>
-              </div>
-              <p> to use the app.</p>
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <div className='items-center flex gap-2'>
+            <p>Hello guest 👋, please </p>
+            <div className='p-2 bg-blue-700 rounded-xl text-white cursor-pointer'>
+              <SignInButton mode="modal">Sign in</SignInButton>
             </div>
-          </SignedOut>
-        </div>
+            <p> to use the app.</p>
+          </div>
+        </SignedOut>
       </div>
     </>
   )
