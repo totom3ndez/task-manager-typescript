a6dd07f25e0b68d2e5d8ec0b40dbecb81e5edbf7
totom3ndez <tomii.mendez45@gmail.com>
2025-05-13 09:47:37 +0200

    feat: update Header component to include user greeting and conditional rendering for sign-out button

--- src/globals/components/Header.tsx
+++ src/globals/components/Header.tsx
@@ -1,10 +1,25 @@
 import { Link } from 'react-router-dom'
+import { useUser, SignedIn, SignOutButton } from '@clerk/clerk-react'
+import Button from './Button'
 
 const Header = () => {
+  const { user } = useUser()
   return (
     <header className='flex justify-between p-4 font-bold items-center'>
       <Link to="/">Task Manager</Link>
-      <Link to="/create-task" className='p-2 bg-blue-700 rounded-xl text-white'>New Task</Link>
+      <SignedIn>
+        <Link to="/create-task" className='p-2 bg-blue-700 rounded-xl text-white'>New Task</Link>
+      </SignedIn>
+      <Button type="button" color='text-white p-2' >
+        Hello, {user ? user.firstName : 'Guest'}
+      </Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button type="button" color='text-red-700 p-2'>
+            Sign Out
+          </Button>
+        </SignOutButton>
+      </SignedIn>
     </header>
   )
 }
