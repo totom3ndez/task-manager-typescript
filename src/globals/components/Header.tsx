c8c7832a55bfd8d3fe0fff0ef480da2a17d5ee99
totom3ndez <tomii.mendez45@gmail.com>
2025-05-13 14:49:53 +0200

    feat: enhance Header component with responsive design and user greeting

--- src/globals/components/Header.tsx
+++ src/globals/components/Header.tsx
@@ -1,22 +1,43 @@
 import { Link } from 'react-router-dom'
 import { useUser, SignedIn, SignOutButton } from '@clerk/clerk-react'
 import Button from './Button'
+import { useEffect, useState } from 'react'
+import { LuLogOut } from "react-icons/lu";
+import { IoMdHome } from "react-icons/io";
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(true)
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+
+
   const { user } = useUser()
   return (
     <header className='flex justify-between p-4 font-bold items-center'>
-      <Link to="/">Task Manager</Link>
+      <Link to="/">{isMobile ? <IoMdHome className='cursor-pointer size-8' /> : 'Task Manager'}</Link>
+      <SignedIn>
+      </SignedIn>
+      <p className='text-center text-white'>
+        Hello, {user ? user.firstName : 'Guest'}
+      </p>
       <SignedIn>
         <Link to="/create-task" className='p-2 bg-blue-700 rounded-xl text-white'>New Task</Link>
-      </SignedIn>
-      <Button type="button" color='text-white p-2' >
-        Hello, {user ? user.firstName : 'Guest'}
-      </Button>
-      <SignedIn>
         <SignOutButton>
           <Button type="button" color='text-red-700 p-2'>
-            Sign Out
+            {isMobile ? <LuLogOut className='cursor-pointer size-8' /> : 'Sign out'}
           </Button>
         </SignOutButton>
       </SignedIn>
